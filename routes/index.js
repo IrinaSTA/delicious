b2230a5b8f9b6ee03250b0da75c1ef98b5d3d2b3
Irina Baldwin <2007inb@gmail.com>
2019-01-15 15:24:55 +0000

    Set up index controller

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here. Make sure to only send data once
-router.get('/', (req, res) => {
-  // const irina = { name: 'Irina', age: 39, cool: true }
-  // res.json(irina)
-  // res.send('Hey! It works!')
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'Irina',
-    // dog is a 'local' variable or a 'local'
-    dog: req.query.dog,
-    title: 'Delicious'
-  })
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/', storeController.homePage);
 
 module.exports = router;
